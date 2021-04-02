5f6cc62a39fa96ae49829053495f0452a7425401
Uniguri <78708465+Uniguri@users.noreply.github.com>
2021-04-03 03:07:26 +0900

    Delete Console.Log
    The title says it all

--- zutomayomain.js
+++ zutomayomain.js
@@ -6,20 +6,12 @@ let count = 0;
 function init(){
     document.getElementById('text').innerText = list[0];
 }
-function resize() {
-    const text = document.getElementById('text');
-    const shadows = document.getElementsByClassName('shadows');
-    text.style.paddingTop = (window.innerHeight - 217) / 2;
-    text.style.paddingBottom = (window.innerHeight - 217) / 2;
-}
 
-window.addEventListener("resize", resize);
 window.addEventListener("wheel", (event) => {
     const text = document.getElementById('text');
     if(event.deltaY > 0)
         count++;
     else
         count--;
-    console.log(count);
     text.innerText = list[Math.abs(count%list.length)];
 })
