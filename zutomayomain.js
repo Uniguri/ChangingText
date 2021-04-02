b6e14fb6be9e251d581dfe0bb4cb690bb7cd0af3
Uniguri <78708465+Uniguri@users.noreply.github.com>
2021-04-03 02:26:37 +0900

    Switch Text
    Center and Middle with Padding

---
+++ zutomayomain.js
@@ -0,0 +1,22 @@
+let list = [
+    "Zutomayo", "Is", "Good"
+]
+let count = 0;
+
+function resize() {
+    const text = document.getElementById('text');
+    const shadows = document.getElementsByClassName('shadows');
+    text.style.paddingTop = (window.innerHeight - 217) / 2;
+    text.style.paddingBottom = (window.innerHeight - 217) / 2;
+}
+
+window.addEventListener("resize", resize);
+window.addEventListener("wheel", (event) => {
+    const text = document.getElementById('text');
+    if(event.deltaY > 0)
+        count++;
+    else
+        count--;
+    console.log(count);
+    text.innerText = list[Math.abs(count%list.length)];
+})
